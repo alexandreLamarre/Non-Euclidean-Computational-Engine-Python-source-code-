ac4e23184180a4ff7bd3b403898722be8799c17e
alexandreLamarre <alex7285@gmail.com>
2020-08-02 14:50:50 -0400

    Fixed syntax and function definitions in PlotFrame

--- GUI/PlotFrame.py
+++ GUI/PlotFrame.py
@@ -1,12 +1,14 @@
 import tkinter as tk
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from src.render_tools import plot_IFS
 
 class PlotFrame(tk.Frame):
     def __init__(self, functions, transitions):
         tk.Frame.__init__(self)
+        self.mat_plot_canvas(functions, transitions)
 
-    def mat_plot_canvas(self):
-        # figure = IFS_plot()
-        canvas = FigureCanvasTkAgg(f, self)
+    def mat_plot_canvas(self, functions, transitions):
+        figure = plot_IFS(functions, transitions)
+        canvas = FigureCanvasTkAgg(figure, self)
         canvas.draw()
         canvas.get_tk_widget().pack()
