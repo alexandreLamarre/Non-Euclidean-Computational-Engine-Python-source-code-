e86e3c7ecfab3dac98793414c1d4bfc226ca6f00
alexandreLamarre <alex7285@gmail.com>
2020-08-04 17:09:05 -0400

    Updated regexes to match commands.

--- src/regex_match.py
+++ src/regex_match.py
@@ -2,10 +2,14 @@ import regex
 
 def match_function(in_str):
     """ Checks whether or not an input string matches the regular expression
-    for a function, in terms of what makes a function usually valid in mathematics
-
+    The string must contain a
+            -function name composed of characters and digits
+            -a set of variables composed of characters separated by ,
+            -a set of output functions with matching parentheses
+    It CANNOT contain a '\'
     Returns an iterable of re.Match objects"""
-    pattern = "(([a-zA-Z]|[0-9])+\s*\({1}([a-zA-Z](,)*)+(\){1})\s*={1})\s*(\((([^\(\)]*)|(?6))*\))"
+
+    pattern = "(([a-zA-Z]|[0-9])+\s*\({1}([a-zA-Z](,)*)+(\){1})\s*={1})\s*(\((([^\(\)\\\\]*)|(?6))*\))"
 
     res = regex.finditer(pattern, in_str)
     return res
@@ -15,3 +19,14 @@ def match_closed_parentheses(in_str):
     return regex.match(pattern, in_str)
 
 
+def match_commands(in_str):
+    pattern = "(\\\\)[a-zA-Z]*\{[^\\\\\{\}]*\}"
+
+    return regex.finditer(pattern, in_str)
+
+
+n = 0
+while(n< 3):
+    s = input()
+    for f in match_commands(s):
+        print(f)
--- tests/regex_test.py
+++ tests/regex_test.py
@@ -1,13 +1,15 @@
 import os
 import regex
-from src.regex_match import match_function, match_closed_parentheses
+from src.regex_match import match_function, match_closed_parentheses, match_commands
 
 
 
 
 if __name__ == "__main__":
-    function_tests_true = ["f(x) = (x,y^2 + s^5)", "f(x,y,z) = (x^2,y*(z+x), amazing*not)"]
+    function_tests_true = ["f(x) = (x,y^2 + s^5)", "f(x,y,z) = (x^2,y*(z+x), amazing*not)","f(x) = (x,y^2 + s^5)" ]
     function_tests_false = ["f(x)", "asdkjhlkasjdklasdjl", "f(x) = "]
+    commands_test_true = ["\\base{asasasas}"]
+    commands_test_false = ["\\ base{\ }"]
 
     bit_false = 0
     errors = 0
@@ -25,15 +27,39 @@ if __name__ == "__main__":
 
     errors = 0
     bit_true = 0
-    print("Start Testing for strings that should be matched to functions...")
+    print("Start Testing for strings that should be matched to functions...\n")
 
     for f in function_tests_true:
         for el in match_function(f):
             bit_true = 1
 
-        if bit_true == 1:
+        if bit_true == 0:
             print("test failed for {}".format(f))
+            errors += 1
         bit_true = 0
-    print("Finished Testing for strings that should be matched to functions, found {} errors".format(errors))
+    print("Finished Testing for strings that should be matched to functions, found {} errors\n".format(errors))
 
+    errors = 0
+    bit_false = 0
+    print("Start Testing for strings that should not be matched to commands...\n")
+    for f in commands_test_true:
+        for el in match_commands(f):
+            bit_false =1
+        if bit_false == 1:
+            print("test failed for {}".format(f))
+            errors += 1
+        bit_false = 0
+
+    print("Finished Testing for strings that should not be matched to functions, found {} errors\n".format(errors))
         # assert(match_function(f) == None)
+    errors = 0
+    bit_true = 0
+    print("Start Testing for strings that should be matched to commands...\n")
+    for f in commands_test_false:
+        for el in match_commands(f):
+            bit_true = 1
+        if bit_true == 0:
+            print("test failed for {}".format(f))
+            errors += 1
+        bit_true = 0
+    print("Finished Testing for strings that should be matched to functions, found {} errors\n".format(errors))
