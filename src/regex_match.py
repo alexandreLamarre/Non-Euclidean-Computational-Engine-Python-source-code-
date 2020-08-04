7b4fb8b999f43ec37409d61abe61a40b20ec9b57
alexandreLamarre <alex7285@gmail.com>
2020-08-04 15:45:19 -0400

    Fixed syntax and function definitions in PlotFrame

---
+++ src/regex_match.py
@@ -0,0 +1,16 @@
+import regex
+
+def match_function(in_str):
+    """ Checks whether or not an input string matches the regular expression
+    for a function, in terms of what makes a function usually valid in mathematics
+
+    Returns an iterable of re.Match objects"""
+    pattern = "(([a-zA-Z]|[0-9])+\s*\({1}([a-zA-Z](,)*)+(\){1})\s*={1})\s*(\((([^\(\)]*)|(?6))*\))"
+
+    res = regex.finditer(pattern, in_str)
+    return res
+
+def match_closed_parentheses(in_str):
+    pattern = "\((([^\(\)]*)|(?R))*\)"
+    return regex.match(pattern, in_str)
+
---
+++ tests/regex_test.py
@@ -0,0 +1,38 @@
+import os
+import regex
+from src.regex_match import match_function, match_closed_parentheses
+
+
+
+
+if __name__ == "__main__":
+    function_tests_true = ["f(x) = (x,y^2 + s^5)", "f(x,y,z) = (x^2,y*(z+x), amazing*not)"]
+    function_tests_false = ["f(x)", "asdkjhlkasjdklasdjl", "f(x) = "]
+
+    bit_false = 0
+    errors = 0
+    print("Start testing for strings that should not be matched to functions... \n")
+    for f in function_tests_false:
+        for el in match_function(f):
+            bit_false =1
+        if bit_false == 1:
+            print("test failed for {}".format(f))
+            errors += 1
+        bit_false = 0
+
+    print("Finished testing for strings that should not be matched to functions, found {} errors \n".format(errors))
+
+    errors = 0
+    bit_true = 0
+    print("Start Testing for strings that should be matched to functions...")
+
+    for f in function_tests_true:
+        for el in match_function(f):
+            bit_true = 1
+
+        if bit_true == 1:
+            print("test failed for {}".format(f))
+        bit_true = 0
+    print("Finished Testing for strings that should be matched to functions, found {} errors".format(errors))
+
+        # assert(match_function(f) == None)
